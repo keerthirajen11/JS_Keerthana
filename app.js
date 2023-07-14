ae61056dacd734ad5fe583c6d77df83754375f17
keerthirajen11 <138966226+keerthirajen11@users.noreply.github.com>
2023-07-14 14:37:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function change(){
+    var img=document.getElementById("bulb");
+    if(img.src.match("off"))
+    {
+        img.src="bulb-electricity-energy-glass.jpg";
+    }
+    else{
+        img.src="realistic-light-bulb-with-electricity_23-2149129410.avif";
+    }
+}
